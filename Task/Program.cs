b76f4dc5802b01031a77dbf1176b927a318856e8
Andrei <aleinik88.aa@gmail.com>
2022-11-14 15:45:30 +0200

    FillArray

---
+++ Task/Program.cs
@@ -0,0 +1,14 @@
+﻿void FillArray(string [] array, string[] array1)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            array1[count] = array[i];
+            count++;
+        }
+    }
+}
+
+
